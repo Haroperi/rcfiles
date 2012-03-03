3546a4b32d63a4112b421871e81f915db01fcd86
Mitsuo NAKAJIMA <haroperitan@gmail.com>
2012-03-03 10:59:23 +0900

    macsetup.sh

---
+++ macsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+brew install cabocha crfpp gd gettext glib gnu-prolog gnuplot imagemagick jasper jpeg kytea libiconv libtiff little-cms lua mecab mecab-ipadic nkf pango pkg-config readline rlwrap sl sqlite tinysvm wget yamcha 
+# brew install git
+
